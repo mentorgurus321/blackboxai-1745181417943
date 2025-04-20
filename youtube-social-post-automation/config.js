02773a0c2e7a49a4d636dd09a1e829239de28a4c
mentorgurus321 <95088448+mentorgurus321@users.noreply.github.com>
2025-04-21 02:07:03 +0530

    Add youtube-social-post-automation/config.js 1745181423094

---
+++ youtube-social-post-automation/config.js
@@ -0,0 +1,30 @@
+// Configuration file for API credentials and tokens
+
+module.exports = {
+  youtubeApiKey: 'YOUR_YOUTUBE_API_KEY',
+
+  facebook: {
+    accessToken: 'YOUR_FACEBOOK_ACCESS_TOKEN',
+    pageId: 'YOUR_FACEBOOK_PAGE_ID',
+  },
+
+  instagram: {
+    accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN',
+    instagramBusinessAccountId: 'YOUR_INSTAGRAM_BUSINESS_ACCOUNT_ID',
+  },
+
+  x: {
+    bearerToken: 'YOUR_X_BEARER_TOKEN',
+  },
+
+  threads: {
+    // Placeholder for Threads API credentials
+  },
+
+  pinterest: {
+    accessToken: 'YOUR_PINTEREST_ACCESS_TOKEN',
+    boardId: 'YOUR_PINTEREST_BOARD_ID',
+  },
+
+  youtubeChannelId: 'YOUR_YOUTUBE_CHANNEL_ID',
+};
